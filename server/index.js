ec8a5cbe6b6d81726076129f8128ff21c41f3c68
gulyaev <gulyaev.gvs@gmail.com>
2022-01-07 17:30:43 +0500

    Create Model User, create express RestAPI /registration

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const authRouter = require("./routes/auth.routes");
+const app = express();
+const PORT = config.get('serverPort');
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.get("dburl"));
+
+        app.listen(PORT, () => {
+            console.log("Server started on port " + PORT);
+        });
+    } catch (error) {
+        
+    }
+}
+
+start();
