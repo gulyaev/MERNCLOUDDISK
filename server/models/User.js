456ea2afa4d2abcf36ff9931972fdfd431fe1931
gulyaev <gulyaev.gvs@gmail.com>
2022-01-16 11:43:26 +0500

    File model. Directory creation

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,7 @@ const User = new Schema({
     diskSpace: {type: String, default: 1024**3*10},
     usedSpace: {type: String, default: 0},
     avatar: {type: String},
-    files: [{type: ObjectId, ref: 'file'}],
+    files: [{type: ObjectId, ref: 'File'}],
 })
 
 module.exports = model('User', User)
