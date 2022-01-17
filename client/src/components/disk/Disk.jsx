3a612022d35207a1eab1bf985054b78cd84947d3
gulyaev <gulyaev.gvs@gmail.com>
2022-01-17 20:16:34 +0500

    Folder navigation

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -4,25 +4,31 @@ import { getFiles, createDir } from "../../actions/file";
 import FileList from "./fileList/FileList";
 import "./disk.less"
 import Popup from "./Popup";
-import {setPopupDisplay} from "../../reducers/fileReducer"
+import {setCurrentDir, setPopupDisplay} from "../../reducers/fileReducer"
 
 const Disk = () => {
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir);
+    const dirStack = useSelector(state => state.files.dirStack);
 
     useEffect(()=>{
-        dispatch(getFiles(currentDir))
+        dispatch(getFiles(currentDir));
+        console.log("dirStack " + dirStack);
     }, [currentDir]);
 
     const showPopupHandler = () => {
-        //dispatch(createDir(currentDir, 'asd'));
         dispatch(setPopupDisplay('flex'));
     }
     
+    const backClickHandler = () => {
+        const backDirId = dirStack.pop();
+        dispatch(setCurrentDir(backDirId));
+    }
+
     return (
         <div className="disk">
             <div className="disk__btns">
-                <button className="disk__back">Назад</button>
+                <button className="disk__back" onClick={()=>backClickHandler()}>Назад</button>
                 <button className="disk__create" onClick = {() => showPopupHandler()}>Создать папку</button>
             </div>
             <FileList />
